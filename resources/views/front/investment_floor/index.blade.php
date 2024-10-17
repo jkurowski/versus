4f54fc82d9469f39700ddf0a21e4693bc1346847
Jacek <kontakt@jk-design.pl>
2024-10-17 14:35:56 +0200

    Garaze

--- resources/views/front/investment_floor/index.blade.php
+++ resources/views/front/investment_floor/index.blade.php
@@ -59,7 +59,9 @@
                                                 data-roomtype="{{$r->typ}}"
                                                 data-roomstatus="{{$r->status}}"
                                                 coords="{{cords($r->html)}}"
-                                                class="inline status-{{$r->status}}" href="@if($r->status <> 3 && $floor->type = 1) {{route('front.investment.property', ['investment_id' => $investment->id, 'floor' => $floor->id, 'property' => $r->id])}} @else # @endif" title="<h4 class='mb-2'>{{$r->name}}</h4><ul class='mb-0 list-unstyled mt-0'><li>Powierzchnia: <b class=fr>{{$r->area}} m<sup>2</sup></b></li><li>Pokoje: <b class=fr>{{$r->rooms}}</b></li><li><b>{{ roomStatus($r->status) }}</b></li></ul>">
+                                                class="inline status-{{$r->status}}"
+                                                href="@if($r->status <> 3 && $floor->type == 1) {{route('front.investment.property', ['investment_id' => $investment->id, 'floor' => $floor->id, 'property' => $r->id])}} @else # @endif"
+                                                title="<h4 class='mb-2'>{{$r->name}}</h4><ul class='mb-0 list-unstyled mt-0'><li>Powierzchnia: <b class=fr>{{$r->area}} m<sup>2</sup></b></li><li>Pokoje: <b class=fr>{{$r->rooms}}</b></li><li><b>{{ roomStatus($r->status) }}</b></li></ul>">
                                     @endif
                                 @endforeach
                             @endif
